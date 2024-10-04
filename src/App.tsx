7ca81ae05008da6840ab54f4f8300202c23e4c44
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-10-04 17:09:11 +0500

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Box } from "@mui/material";
 import React from "react";
+import Home from "./app/pages/home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Box>
+      <Home />
+    </Box>
+  );
 }
 
 export default App;
